31c3548760e83f3cae802d13df6e3a97139da092
Kristian Mandrup <kmandrup@gmail.com>
2017-09-09 13:50:22 +0200

    more tests

---
+++ test/new/nodes/registry.test.js
@@ -0,0 +1,20 @@
+import ava from 'ava'
+
+test('NodesRegistry: create', t => {
+
+})
+
+test('registry: setModulePendingUpdated', t => {})
+test('registry: getModule', t => {})
+test('registry: getNodeSetForType', t => {})
+test('registry: getModuleList', t => {})
+test('registry: getNodeList', t => {})
+test('registry: getNodeTypes', t => {})
+test('registry: setNodeList', t => {})
+test('registry: removeNodeSet', t => {})
+test('registry: addNodeSet', t => {})
+test('registry: getNodeSet', t => {})
+test('registry: enableNodeSet', t => {})
+test('registry: disableNodeSet', t => {})
+test('registry: registerNodeType', t => {})
+// ...
